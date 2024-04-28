306b7f71c9015ef4018eb5c5b9c2282213d9182d
Maciej Grzybacz <128727060+MaciejGrzybacz@users.noreply.github.com>
2024-04-28 21:27:59 +0200

    update

--- DataBases/MergeTwoTables.py
+++
@@ -1,8 +0,0 @@
-# https://leetcode.com/problems/combine-two-tables/
-
-import pandas as pd
-
-def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
-    merged_frames=pd.merge(person,address,on='personId',how='left')
-    merged_frames= merged_frames.drop(columns=['personId','addressId'])
-    return merged_frames
